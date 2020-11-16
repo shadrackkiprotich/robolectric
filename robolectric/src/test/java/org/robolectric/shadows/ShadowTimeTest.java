b924d550a891503776e81b4a4ab0c13f69f1b1d6
brettchabot <brettchabot@google.com>
2020-11-16 11:54:02 -0800

    Remove CUR_DEVELOPMENT mistakenly added during debugging
    PiperOrigin-RevId: 342686735

--- robolectric/src/test/java/org/robolectric/shadows/ShadowTimeTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowTimeTest.java
@@ -1,19 +1,12 @@
 package org.robolectric.shadows;
 
-import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR2;
 import static android.os.Build.VERSION_CODES.KITKAT_WATCH;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 import android.os.SystemClock;
 import android.text.format.Time;
-import android.util.TimeFormatException;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import java.util.TimeZone;
-import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
